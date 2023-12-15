b7315d287942238fa444ceafbfbd5ea073f39834
indranil <indranilnonofficial007@gmail.com>
2023-12-16 00:36:24 +0530

    ui update 15-12-2023 7

--- client/src/component/Galllery/Gallery.jsx
+++ client/src/component/Galllery/Gallery.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import CommonHeader from '../CommonHeader';
 
+import CommonHeader from '../CommonHeader';
+import img1 from './GalleryImage/IMG20230912051244.png';
 import Footer from '../Home/Footer';
 
 const Gallery = (props) => {
+    const images = [
+        { src: {img1}, alt: 'Image 1' },
+        { src: 'image2.jpg', alt: 'Image 2' },
+        { src: 'image3.jpg', alt: 'Image 3' },
+        { src: 'image4.jpg', alt: 'Image 4' },
+        { src: 'image5.jpg', alt: 'Image 5' }
+      ];
     return (
+        
         <div>
             <CommonHeader/>
-            Gallery
+            <div className='gallery'>
+            {images.map((image, index) => (
+                <img
+                 key={index}
+                 className="gallery__item"
+                src={image.src}
+                alt={image.alt}
+        />
+      ))}
+
+            </div> 
             <Footer/>
         </div>
        
