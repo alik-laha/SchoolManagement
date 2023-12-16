a23a99b14fdc3c363819d1eeef73ebc90528639c
Alik Laha <aliklaha0@gmail.com>
2023-12-16 09:51:52 +0530

    fixed routing

--- client/src/component/Home/DropdownMenu.jsx
+++ client/src/component/Home/DropdownMenu.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-
 const DropdownMenu = (prop) => {
   return (
-    <div className="dropdown-menu">{prop.props.name}
-
+    <div className="dropdown-menu">
+      {prop.props.name}
       <ul>
-      <li><NavLink to ={'/'+prop.props.history}>History</NavLink></li>
-        
-        <li><NavLink to ="{prop.props.vm}">Vision & Mission</NavLink></li>
-        <li><NavLink to ='{prop.props.programs}'>Programs </NavLink></li>
-        <li><NavLink to ='{prop.props.facilities}'>Facilities</NavLink></li>
+        <li>
+          <NavLink to={"/" + prop.props.value.history}>History</NavLink>
+        </li>
+
+        <li>
+          <NavLink to={prop.props.value.vm}>Vision & Mission</NavLink>
+        </li>
+        <li>
+          <NavLink to={prop.props.value.programs}>Programs </NavLink>
+        </li>
+        <li>
+          <NavLink to={prop.props.value.facilities}>Facilities</NavLink>
+        </li>
       </ul>
     </div>
   );
