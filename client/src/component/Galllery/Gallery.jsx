03b1c2b729f697a3b67229c175baab162905d97d
indranil <indranilnonofficial007@gmail.com>
2023-12-17 12:07:15 +0530

    ui update 17-12-2023 gallery-final-4

--- client/src/component/Galllery/Gallery.jsx
+++ client/src/component/Galllery/Gallery.jsx
@@ -3,7 +3,18 @@ import CommonHeader from '../CommonHeader';
 import Footer from '../Home/Footer';
 import img1 from './GalleryImage/img1.jpg'
 import img3 from './GalleryImage/img3.jpg'
-import img4 from './GalleryImage/IMG20230815170241.jpg'
+import img4 from './GalleryImage/img4.jpg'
+import img5 from './GalleryImage/img5.jpg'
+import img6 from './GalleryImage/img6.jpeg'
+
+import img8 from './GalleryImage/img8.jpeg'
+import img9 from './GalleryImage/img9.jpeg'
+import img10 from './GalleryImage/img10.jpeg'
+import img11 from './GalleryImage/img11.jpeg'
+import img12 from './GalleryImage/img12.jpg'
+
+
+
 
 import imgline from './GalleryImage/line.png'
 import '../Home/Home.css'
@@ -13,8 +24,17 @@ import '../Home/Home.css'
 const Gallery = () => { 
     const slides = [
     <img src={img1} alt="Slide 1" />,
-    <img src={img3} alt="Slide 2"  />,
-    <img src={img4} alt="Slide 3"  />,
+    <img src={img3} alt="Slide 3"  />,
+    <img src={img4} alt="Slide 4"  />,
+    <img src={img5} alt="Slide 5"  />,
+    <img src={img6} alt="Slide 6"  />,
+    
+    <img src={img8} alt="Slide 8"  />,
+    <img src={img9} alt="Slide 9"  />,
+    <img src={img10} alt="Slide 10"  />,
+    <img src={img11} alt="Slide 11"  />,
+    <img src={img12} alt="Slide 12"  />,
+    
   ];
 
   return (
