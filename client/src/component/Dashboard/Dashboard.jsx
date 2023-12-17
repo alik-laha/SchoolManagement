957d5d7d9766be9743a23ea3c131e70e8d7debc2
indranil <indranilnonofficial007@gmail.com>
2023-12-17 16:49:59 +0530

    dashboard visibility 3 added

--- client/src/component/Dashboard/Dashboard.jsx
+++ client/src/component/Dashboard/Dashboard.jsx
@@ -3,8 +3,8 @@ import DashBoardMain from "../DashboardMain/DashBoardMain";
 import "./Dashboard.css";
 import Footer from "../Home/Footer";
 import icon from '../Galllery/GalleryImage/operater_icon.png';
-import { useEffect, useState } from "react";
-import { CgProfile } from "react-icons/cg";
+import {useState } from "react";
+
 
 const Dashboard = () => {
   const HandaleLogout = () => {
@@ -22,16 +22,20 @@ const Dashboard = () => {
   return (
     <>
       <div className='dashoboard-main-header'>
-        <span className="logo-lg">AL-HILAL DASHBOARD</span>
-        <span onClick={logoutVisiblity} className="" ><h4><CgProfile /></h4></span>
+        <span className="logo-lg">DASHBOARD</span>
+        <span className='operator-hide' onClick={logoutVisiblity}><img style={{height:'40px',width:'40px'}}src={icon}></img></span>
         <span  className='navbar-custom-menu'>
           <ul className="dropdown-menu" style={{ display: dashbvisi}}>
             <li className="user-header">
               <span style={{textAlign:'-webkit-center'}}><img src={icon} alt='Operator Icon' className="img-header"></img></span>
-              <p className="text-logout ">Welcome {sessionStorage.getItem("name")}</p>
+              <p className="text-logout">Welcome {sessionStorage.getItem("name")}</p>
             </li>
             
-            <li className="user-footer"><span style={{float:'right'}}><button className='dashboard-btn dashboard-btn-default' onClick={HandaleLogout}>Logout</button></span></li>
+            <li className="user-footer">
+              <span style={{float:'right'}}>
+              <button className='dashboard-btn dashboard-btn-default' onClick={HandaleLogout}>Logout</button>
+              </span>
+            </li>
           </ul>
           
         </span>
