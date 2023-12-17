4d14a3d4544e63900cde26620b3c3442e903c9f5
indranil <indranilnonofficial007@gmail.com>
2023-12-17 16:21:40 +0530

    dashboard visibility added

--- client/src/component/Dashboard/Dashboard.jsx
+++ client/src/component/Dashboard/Dashboard.jsx
@@ -2,20 +2,30 @@ import SideBar from "../sideBar/SideBar";
 import DashBoardMain from "../DashboardMain/DashBoardMain";
 import "./Dashboard.css";
 import Footer from "../Home/Footer";
-import icon from '../Galllery/GalleryImage/operater_icon.png'
+import icon from '../Galllery/GalleryImage/operater_icon.png';
+import { useEffect, useState } from "react";
+import { CgProfile } from "react-icons/cg";
 
 const Dashboard = () => {
   const HandaleLogout = () => {
     sessionStorage.removeItem("user");
     window.location.reload();
   };
+ const [dashbvisi,setdashBVisi]=useState('none');
+ const logoutVisiblity = () => {
+  if (dashbvisi === "none") {
+    setdashBVisi("block");
+  } else {
+    setdashBVisi("none");
+  }
+}
   return (
     <>
       <div className='dashoboard-main-header'>
         <span className="logo-lg">AL-HILAL DASHBOARD</span>
-        <span className='navbar-custom-menu'>
-        
-          <ul className="dropdown-menu">
+        <span onClick={logoutVisiblity} className="" ><h4><CgProfile /></h4></span>
+        <span  className='navbar-custom-menu'>
+          <ul className="dropdown-menu" style={{ display: dashbvisi}}>
             <li className="user-header">
               <span style={{textAlign:'-webkit-center'}}><img src={icon} alt='Operator Icon' className="img-header"></img></span>
               <p className="text-logout ">Welcome {sessionStorage.getItem("name")}</p>
