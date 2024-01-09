0893cda0fc0578b599c17807a16dc684a496c159
Alik Laha <aliklaha0@gmail.com>
2024-01-09 21:27:30 +0530

    nothing

--- MiddleLayer/controller/userController.js
+++ MiddleLayer/controller/userController.js
@@ -3,29 +3,34 @@ const fs = require('fs');
 
 //login user
 exports.Login = (req, res) => {
-    const {name, pass} = req.body
-    if (!{name, pass}) {
-        return res.message("all data needed")
-    }
-            query = `
-                SELECT *
-                FROM user
-                WHERE user_name = "${name}" AND password = "${pass}"`;
-            Database.query(query, function (error, data) {
-                if (error) {
-                    return res.status(400).json({
-                        status: "failed",
-                        data: "user not found"
-                    });
-                }
-                if (data) {
-                        return res.status(200).json({
-                            status: "success",
-                            data: data
-                        });
-                    }
+    try {
+        const {name, pass} = req.body
+        if (!{name, pass}) {
+            return res.message("all data needed")
+        }
+        query = `
+            SELECT *
+            FROM user
+            WHERE user_name = "${name}"
+              AND password = "${pass}"`;
+        Database.query(query, function (error, data) {
+            if (error) {
+                return res.status(400).json({
+                    status: "failed",
+                    data: "user not found"
+                });
+            }
+            if (data) {
+                return res.status(200).json({
+                    status: "success",
+                    data: data
+                });
+            }
 
-            })
+        })
+    }catch(err){
+        console.log(err.message)
+    }
 
 }
 
