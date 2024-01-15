b25666feb69e18a0a3a47015c3c9e2c60331ddb7
Alik Laha <aliklaha0@gmail.com>
2024-01-15 20:07:30 +0530

    hostel entry

--- client/src/component/HOSTEL/HostelEntry/HostelEntry.jsx
+++ client/src/component/HOSTEL/HostelEntry/HostelEntry.jsx
@@ -1,7 +1,14 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 const HostelEntry = () => {
-const [studentData, setStudentData] = useState([])
+    const [id, setId] = useState("");
+    const [studentName, setStudentName] = useState("");
+    const [regNo, setRegNo] = useState("");
+    const [roomNo, setRoomNo] = useState("");
+    const [bedNo, setBedNo] = useState("");
+    const [entryDate, setEntryDate] = useState("");
+    const [studentData, setStudentData] = useState([])
+
     useEffect(() => {
        axios.get("http://localhost:7000/api/v1/student/getallstudent")
               .then((res) => {
@@ -13,17 +20,40 @@ const [studentData, setStudentData] = useState([])
     }, []);
     return(
         <>
-            <div>
-                {
-                    studentData.map((student) => {
-                        return(
-                            <div key={student.student_id}>
-                                {console.log(student)}
-                            </div>
-                        )
-                    })
-                }
-            </div>
+                <div>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Student Name</th>
+                            <th>Regidtration No</th>
+                            <th>Entry status</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        {
+                            studentData.map((data)=> {
+                                return(
+                                <tbody key={data.student_id}>
+                                <tr>
+                                    <td>{data.student_id}</td>
+                                    <td>{data.student_Name}</td>
+                                    <td>{data.registration_no}</td>
+                                    <td></td>
+                                    <td>
+                                        <button>Entry</button>
+                                    </td>
+                                    {console.log(data)}
+
+                                </tr>
+                                </tbody>
+                                )
+
+                            })
+                        }
+
+                    </table>
+                </div>
         </>
     )
 }
