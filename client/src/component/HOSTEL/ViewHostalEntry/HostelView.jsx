28a9a9e81c0ed69d76b5af56cca38df475940cef
Alik Laha <aliklaha0@gmail.com>
2024-01-17 19:22:50 +0530

    hostel entry work in pogress

---
+++ client/src/component/HOSTEL/ViewHostalEntry/HostelView.jsx
@@ -0,0 +1,25 @@
+import {useState} from "react";
+
+const HostelView= (props) => {
+
+    return(
+        <>
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Student Name</th>
+                        <th>Registration No</th>
+                        <th>Room No</th>
+                        <th>Bed No</th>
+                        <th>Entry Date</th>
+                    </tr>
+                    </thead>
+
+                </table>
+            </div>
+        </>
+    )
+}
+export default HostelView;
