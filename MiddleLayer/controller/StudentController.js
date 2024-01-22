7951e3db58cb1e438c4959de18cd661e73c63884
Alik Laha <aliklaha0@gmail.com>
2024-01-22 18:01:16 +0530

    Master student in pogress

--- MiddleLayer/controller/StudentController.js
+++ MiddleLayer/controller/StudentController.js
@@ -7,48 +7,48 @@ const Database = require('../Config/Dbconnection')
 
 
 //get all students byclass and registration
-exports.GetAllStudent=(req,res)=>{
-    const {Class,regNo,year}=req.body
+exports.GetAllStudent = (req, res) => {
+    const { Class, regNo, year } = req.body
     let query
-    try{
-        if(!Class && !regNo && !year){
-            query=`SELECT * FROM Student_Admission where active=1`
+    try {
+        if (!Class && !regNo && !year) {
+            query = `SELECT * FROM Student_Admission where active=1`
         }
-        else if(Class && !regNo && !year){
-            query=`SELECT * FROM Student_Admission WHERE Class='${Class}' and active=1`
-        }
-
-        else if(!Class && regNo && !year){
-            query=`SELECT * FROM Student_Admission WHERE registration_no regexp '${regNo}' and active=1`
-        }
-        else if(!Class && !regNo && year){
-            query=`SELECT * FROM Student_Admission WHERE admission_year regexp '${year}' and active=1`
+        else if (Class && !regNo && !year) {
+            query = `SELECT * FROM Student_Admission WHERE Class='${Class}' and active=1`
         }
 
-        else if(Class && regNo && !year){
-            query=`SELECT * FROM Student_Admission WHERE Class regexp '${Class}' and registration_no regexp '${regNo}' and active=1`
+        else if (!Class && regNo && !year) {
+            query = `SELECT * FROM Student_Admission WHERE registration_no regexp '${regNo}' and active=1`
         }
-        else if(!Class && regNo && year){
-            query=`SELECT * FROM Student_Admission WHERE registration_no regexp '${regNo}' and admission_year regexp '${year}' and active=1`
-        }
-        else if(Class && !regNo && year){
-            query=`SELECT * FROM Student_Admission WHERE Class regexp '${Class}' and admission_year regexp '${year}' and active=1`
+        else if (!Class && !regNo && year) {
+            query = `SELECT * FROM Student_Admission WHERE admission_year regexp '${year}' and active=1`
         }
 
-        else{
-            query=`SELECT * FROM Student_Admission WHERE registration_no regexp '${regNo}' and admission_year regexp '${year}' and Class regexp '${Class}' and active=1`
+        else if (Class && regNo && !year) {
+            query = `SELECT * FROM Student_Admission WHERE Class regexp '${Class}' and registration_no regexp '${regNo}' and active=1`
         }
-       
-        Database.query(query,(err,result)=>{
-            if(err){
+        else if (!Class && regNo && year) {
+            query = `SELECT * FROM Student_Admission WHERE registration_no regexp '${regNo}' and admission_year regexp '${year}' and active=1`
+        }
+        else if (Class && !regNo && year) {
+            query = `SELECT * FROM Student_Admission WHERE Class regexp '${Class}' and admission_year regexp '${year}' and active=1`
+        }
+
+        else {
+            query = `SELECT * FROM Student_Admission WHERE registration_no regexp '${regNo}' and admission_year regexp '${year}' and Class regexp '${Class}' and active=1`
+        }
+
+        Database.query(query, (err, result) => {
+            if (err) {
                 console.log(err)
             }
-            else{
-                return res.status(200).json({result})
+            else {
+                return res.status(200).json({ result })
             }
         })
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 }
@@ -56,7 +56,7 @@ exports.GetAllStudent=(req,res)=>{
 
 //Master student admission
 
-exports.MasterStudentAdmission=(req,res)=> {
+exports.MasterStudentAdmission = (req, res) => {
     const {
         name,
         regNo,
@@ -97,12 +97,16 @@ exports.MasterStudentAdmission=(req,res)=> {
                 msg: "please fill all the importent field"
             })
         } else {
+            let query = `
+INSERT INTO master_student(student_Name,adhar_no,registration_no,sex,religion,dob,cast,physically_challenged,orphanage,father_name,father_qualification,father_ocupation,
+    father_monthlyIncome,father_contact,mother_name,mother_qualification,mother_ocupation,mother_monthlyIncome,mother_contact,guardian_name,relationship,guardian_contact,address,pin_no,bsp_id,applied_class,admission_year,admission_date,age,blood_group,account_no,branch,ifsc)
+    VALUES ("${name}","${adharNo}","${regNo}" )`
 
         }
     }
-    catch(err){
+    catch (err) {
         return res.status(500).json({
-            err:err
+            err: err
         })
     }
 }
--- MiddleLayer/router/StudentRouter.js
+++ MiddleLayer/router/StudentRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {GetAllStudent} = require('../controller/StudentController')
+const {GetAllStudent,MasterStudentAdmission} = require('../controller/StudentController')
 
 //get all students
 router.post('/getallstudent',GetAllStudent)
-//
+
+//Master student admission
+router.post('/masterstudentadmission',MasterStudentAdmission)
 
 
 module.exports = router
