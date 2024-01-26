47b04533d8b21cf4b4c0f4f293dd2d9ebec4d4c3
Alik Laha <aliklaha0@gmail.com>
2024-01-26 23:49:42 +0530

    Faculty in pogrress

--- MiddleLayer/controller/FacultyController.js
+++ MiddleLayer/controller/FacultyController.js
@@ -25,3 +25,72 @@ exports.createFaculty = (req, res) => {
         res.status(400).json({message:"Error Occured",err:err})
     }
 }
+
+//delete faculty
+exports.DeleteFaculty = (req, res) => {
+    const {id}=req.body
+    try{
+        if(id){
+            let query = `DELETE FROM faculty_admin WHERE id=${id}`
+            Database.query(query,(err,result)=>{
+                if(err){
+                    console.log(err)
+                    res.status(400).json({message:"Error Occured"})
+                }
+                else{
+                    res.status(200).json({message:"Faculty Deleted"})
+                }
+            })
+        }
+        else {
+            res.status(400).json({message:"All Fields are required"})
+        }
+    }catch (err){
+        console.log(err)
+        res.status(400).json({message:"Error Occured",err:err})
+    }
+}
+
+//update faculty
+exports.UpdateFaculty = (req, res) => {
+    const {id,name,qualification,joinDate,email,specialized,contactNo}=req.body
+    try{
+        if(id && name && qualification && joinDate && email && specialized && contactNo){
+            let query = `UPDATE faculty_admin SET name='${name}',heighst_qualification='${qualification}',join_date='${joinDate}',email='${email}',specialized_field='${specialized}',contact_no='${contactNo}' WHERE id=${id}`
+            Database.query(query,(err,result)=>{
+                if(err){
+                    console.log(err)
+                    res.status(400).json({message:"Error Occured"})
+                }
+                else{
+                    res.status(200).json({message:"Faculty Updated"})
+                }
+            })
+        }
+        else {
+            res.status(400).json({message:"All Fields are required"})
+        }
+    }catch (err){
+        console.log(err)
+        res.status(400).json({message:"Error Occured",err:err})
+    }
+}
+
+//get all faculty
+exports.GetAllFaculty = (req, res) => {
+    try{
+        let query = `SELECT * FROM faculty_admin WHERE active=1`
+        Database.query(query,(err,result)=>{
+            if(err){
+                console.log(err)
+                res.status(400).json({message:"Error Occured"})
+            }
+            else{
+                res.status(200).json({message:"Faculty Fetched",data:result})
+            }
+        })
+    }catch (err){
+        console.log(err)
+        res.status(400).json({message:"Error Occured",err:err})
+    }
+}
