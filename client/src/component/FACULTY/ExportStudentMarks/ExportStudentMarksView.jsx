5491f9320947c02872e621c258821d44accd0229
indranil <indranilnonofficial007@gmail.com>
2024-02-05 13:47:39 +0530

    dob column removed

--- client/src/component/FACULTY/ExportStudentMarks/ExportStudentMarksView.jsx
+++ client/src/component/FACULTY/ExportStudentMarks/ExportStudentMarksView.jsx
@@ -5,6 +5,42 @@ import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 
 const ExportStudentMarksView= (props) => {
 
+    const [view,setView]=useState("none")
+    const [data,setData]=useState([])
+
+    useEffect(()=>{
+        setData(props.data)
+
+    },[props.data])
+
+    // props.data.length>0
+    useEffect(() => {
+        if( props.view==="block" && props.view40==="block"){
+            setView("block")
+        }
+        else {
+            setView("none")
+        }
+    }, [props.data,props.view,props.view40]);
+
+    const clearTable = () => {
+        if(hideView==='none')
+        setData([]);
+      };
+
+
+
+      return(
+        <div style={{display: view}}>
+            ASS
+            <table className="table-60"></table>
+        
+        
+        </div>
+
+
+
+      )
     
 }
 export default ExportStudentMarksView;
