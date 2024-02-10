71dec0278a12f90db79220e48223411572c36e0d
Alik Laha <aliklaha0@gmail.com>
2024-02-10 16:58:28 +0530

    stock usage

--- client/src/component/PromoteNextClass/PromoteNextClassView.jsx
+++ client/src/component/PromoteNextClass/PromoteNextClassView.jsx
@@ -20,12 +20,12 @@ const PromoteNextClassView= (props) => {
         if(props.SearchebyData.length<=0)
         {
             setPromotehide('none')
-            
-            
+
+
         }
         else{
             setPromotehide('inline-block')
-            
+
         }
       },[props.SearchebyData])
 
@@ -42,14 +42,48 @@ const PromoteNextClassView= (props) => {
        
     },[props.PromoteView,props.view,props.SearchebyData])
 
-    const HandleEdit=(academicAll)=> {
-        
-        let count=0;
-        let data_length=academicAll.length
-        if (Class < 12){
-          
-            
-            academicAll.map((data) => {
+
+    const handaleSubmit=()=>{
+        const data={
+            Class,
+            academicYear,
+        }
+        axios.post("http://localhost:7000/api/v1/student/getpromotesearch",data)
+            .then((res)=>{
+                console.log(res.data.result)
+               setAcademicAll(res.data.result)
+
+            })
+            .catch((error)=>{
+                console.log(error)
+            } )
+
+    }
+
+
+      const handlePass=(data)=>{
+          console.log(data)
+          if(data.class===12){
+              axios.post("http://localhost:7000/api/v1/student/updatepromotion", {
+                  Class: data.class,
+                  regNo: data.registration_no,
+                  academicYear: data.current_academic_year
+              })
+                  .then((res) => {
+                      axios.post("http://localhost:7000/api/v1/hostel/deletehostelentrybyregno", {regNo: data.registration_no})
+                            .then((res) => {
+                                console.log("done")
+                                alert(`Student ${data.student_Name} has been Promoted to Next Class and Academic Year `)
+                                handaleSubmit()
+                            }).catch((error)=>{
+                                console.log(error)
+                            })
+
+                  }).catch((error)=>{
+                      console.log(error)
+                  })
+              }
+          else{
                 axios.post("http://localhost:7000/api/v1/student/updatepromotion", {
                     Class: data.class,
                     regNo: data.registration_no,
@@ -57,46 +91,18 @@ const PromoteNextClassView= (props) => {
                 })
                     .then((res) => {
                         console.log("done")
-                        count=count+1
-                        if(count===data_length){
-                            alert(`All the Students of Class ${Class} and Academic Year ${academicYear} has been Promoted to Next Class and Academic Year `)
-                            setView('none')
-                            
-                        }
-                      
-                       
+                        alert(`Student ${data.registration_no} has been Promoted to Next Class and Academic Year `)
+                        handaleSubmit()
+                    }).catch((error)=>{
+                        console.log(error)
                     })
-            })
-          
 
+          }
+          }
+        const handleFail=()=>{
+            handaleSubmit()
+        }
 
-    }
-    else
-    {
-        
-        academicAll.map((data) => {
-            axios.post("http://localhost:7000/api/v1/student/updatepromotion", {regNo: data.registration_no})
-                .then((res) => {
-                    axios.post("http://localhost:7000/api/v1/hostel/deletehostelentrybyregno", {regNo: data.registration_no})
-                    .then((res) => {
-                        console.log("done")
-                        count=count+1
-                        if(count===data_length){
-                            alert(`All the Students of Class ${Class} and Academic Year ${academicYear} has been Promoted to Next Class and Academic Year `)
-                            setView('none')
-                            
-                        }
-                    
-                       
-                    })
-                })
-                
-        })
-        
-
-
-    }
-            }
     return(
         <>
         
@@ -114,7 +120,7 @@ const PromoteNextClassView= (props) => {
                         <th>Section</th>
                         <th>Roll No</th>
                         <th>Year of Admission</th>
-                        
+                        <th>Action</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -130,6 +136,10 @@ const PromoteNextClassView= (props) => {
                                     <td>{data.section}</td>
                                     <td>{data.roll_no}</td>
                                     <td>{data.admission_year}</td>
+                                    <td>
+                                        <button onClick={()=>handlePass(data)}  className="dashboard-btn dashboard-btn-scss">Pass</button>
+                                        <button onClick={()=>handleFail(data)} className="dashboard-btn btn-warning">Fail</button>
+                                    </td>
                                 </tr>
                             )
 
@@ -138,20 +148,9 @@ const PromoteNextClassView= (props) => {
                     </tbody>
                    
                 </table>
-                {academicAll.length===0 ? <div className="no-data">No Data Exists</div> : null}
-                <div style={{display:'block',textAlign:'center'}}>
-                <button  style={{display:promotehide}} className="dashboard-btn btn-warning" onClick={() => {
-                                        const confirmBox = window.confirm(
-                                            "Do you really want to Promote these Students to next Class ?"
-                                        );
-                                        if (confirmBox === true) {
-                                            HandleEdit(academicAll);
-                                        }
-                                    }}>Promote To Next Class</button></div>
-                
-                
+
             </div>
-            
+
             {/*{props.data.length===0 ? <div className="no-data">No Data Found</div> : null}*/}
         </>
     )
