fa03377594538f2530557c8a57e80062f832f13a
indranil <indranilnonofficial007@gmail.com>
2024-02-15 00:08:08 +0530

    ui change

--- client/src/component/PromoteNextClass/PromoteNextClassView.jsx
+++ client/src/component/PromoteNextClass/PromoteNextClassView.jsx
@@ -15,6 +15,19 @@ const PromoteNextClassView= (props) => {
     setAcademicYear(props.AcademicYear)
   },[props.Class,props.AcademicYear])
 
+  function convertToRoman(num) {
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
+    roman = '',
+    i;
+    for ( i in lookup ) {
+      while ( num >= lookup[i] ) {
+        roman += i;
+        num -= lookup[i];
+      }
+    }
+    return roman;
+  }
+
     useEffect(()=>{
         setAcademicAll(props.SearchebyData)
         if(props.SearchebyData.length<=0)
@@ -142,7 +155,7 @@ const PromoteNextClassView= (props) => {
                                     <td>{data.student_Name}</td>
                                     <td>{data.registration_no}</td>
                                     <td>{data.current_academic_year}</td>
-                                    <td>{data.class}</td>
+                                    <td>{convertToRoman(data.class)}</td>
                                     <td>{data.section}</td>
                                     <td>{data.roll_no}</td>
                                     
