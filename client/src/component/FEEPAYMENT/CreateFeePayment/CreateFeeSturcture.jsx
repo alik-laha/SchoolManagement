7f5c607c5553cd28f9883c4c6e7501cdbf50d8e0
Alik Laha <aliklaha0@gmail.com>
2024-02-16 11:57:34 +0530

    create fee structure on pogress

--- client/src/component/FEEPAYMENT/CreateFeePayment/CreateFeeSturcture.jsx
+++ client/src/component/FEEPAYMENT/CreateFeePayment/CreateFeeSturcture.jsx
@@ -1,9 +1,26 @@
 import {useState} from "react";
 
 const CreateFeeSturcture = (props) => {
+    const [Class,setClass]=useState("")
+
     return(
         <div style={{display:props.view}}>
-            <h1>Create Fee Structure</h1>
+           <form className="dashbrd-40-colm">
+               <div>
+                     <label>Class</label>
+                     <input type="text" value={Class} onChange={(e)=>setClass(e.target.value)} />
+               </div>
+               <div>
+                   <label>Payment Type</label>
+                     <select>
+                         <option value="">Select</option>
+                          <option value="Monthly">Monthly</option>
+                          <option value="Yearly">Yearly</option>
+                            <option value="ReAdmisson">Re-Admisson</option>
+                     </select>
+               </div>
+
+           </form>
         </div>
     )
 }
