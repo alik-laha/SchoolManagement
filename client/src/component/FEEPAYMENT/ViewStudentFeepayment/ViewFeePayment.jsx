e0f893aa7d6c857919c977e2fc01135df6898fe7
Alik Laha <aliklaha0@gmail.com>
2024-02-24 20:04:25 +0530

    fee payement Entry and update in pogress

--- client/src/component/FEEPAYMENT/ViewStudentFeepayment/ViewFeePayment.jsx
+++ client/src/component/FEEPAYMENT/ViewStudentFeepayment/ViewFeePayment.jsx
@@ -4,7 +4,47 @@ const ViewFeePayment =(props)=>{
     const [view,setView]=useState("none")
     const [data,setData]=useState([])
 
+    const [AdmissonFee,setAdmissonFee]=useState(0)
+    const [hostelCharge,setHostelCharge]=useState(0)
+    const [TutionFee,setTutionFee]=useState(0)
+    const [CautionMoney,setCautionMoney]=useState(0)
+    const [ExaminationFee,setExaminationFee]=useState(0)
+    const [GamesSportsExicursion,setGamesSportsExicursion]=useState(0)
+    const [ElectricCharge,setElectricCharge]=useState(0)
+    const [LibraryFees,setLibraryFees]=useState(0)
+    const [ComputerFees,setComputerFees]=useState(0)
+    const [DevelopmentFees,setDevelopmentFees]=useState(0)
+    const [Miscellaneous,setMiscellaneous]=useState(0)
+    const [LaundryCharge,setLaundryCharge]=useState(0)
+    const [MedicalCharge,setMedicalCharge]=useState(0)
+    const [Uniform,setUniform]=useState(0)
+    const [SessionCharge,setSessionCharge]=useState(0)
+    const [BedFee,setBedFee]=useState(0)
+    const [Total,setTotal]=useState(0)
 
+    const [EditAdmissonFee,setEditAdmissonFee]=useState(0)
+    const [EdithostelCharge,setEditHostelCharge]=useState(0)
+    const [EditTutionFee,setEditTutionFee]=useState(0)
+    const [EditCautionMoney,setEditCautionMoney]=useState(0)
+    const [EditExaminationFee,setEditExaminationFee]=useState(0)
+    const [EditGamesSportsExicursion,setEditGamesSportsExicursion]=useState(0)
+    const [EditElectricCharge,setEditElectricCharge]=useState(0)
+    const [EditLibraryFees,setEditLibraryFees]=useState(0)
+    const [EditComputerFees,setEditComputerFees]=useState(0)
+    const [EditDevelopmentFees,setEditDevelopmentFees]=useState(0)
+    const [EditMiscellaneous,setEditMiscellaneous]=useState(0)
+    const [EditLaundryCharge,setEditLaundryCharge]=useState(0)
+    const [EditMedicalCharge,setEditMedicalCharge]=useState(0)
+    const [EditUniform,setEditUniform]=useState(0)
+    const [EditSessionCharge,setEditSessionCharge]=useState(0)
+    const [EditBedFee,setEditBedFee]=useState(0)
+    const [EditTotal,setEditTotal]=useState(0)
+
+
+
+    const handleEdit=(data)=>{
+        console.log(data)
+    }
     useEffect(() => {
         if(props.view==="block" && props.data.length>0){
             setView("block")
@@ -20,7 +60,57 @@ const ViewFeePayment =(props)=>{
 
     return(
         <div style={{display:view}}>
-            <h1>NoNe</h1>
+            <table className="table-60">
+                <thead>
+                <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Class
+                </th>
+                <th>
+                    Year
+                </th>
+                <th>
+                    Registation No
+                </th>
+                <th>
+                    Pending Amount
+                </th>
+                    <th>
+                        Action
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map((item,index)=>{
+                    const pendingAmount=item.status-item.total_fee
+                    return(
+                        <tr key={index}>
+                            <td>
+                                {item.student_Name}
+                            </td>
+                            <td>
+                                {item.class}
+                            </td>
+                            <td>
+                                {item.year}
+                            </td>
+                            <td>
+                                {item.regNo}
+                            </td>
+                            <td>
+                                {pendingAmount}
+                            </td>
+                            <td>
+                                <button onClick={()=>handleEdit(item)} className="dashboard-btn dashboard-btn-scss">Edit</button>
+                            </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
         </div>
     )
 }
