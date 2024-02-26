bd348b443c6214915399c0451035e8bf25062524
indranil <indranilnonofficial007@gmail.com>
2024-02-26 18:13:22 +0530

    db query updated

--- client/src/component/FACULTY/CreateAndViewSubject/CreateSubject.jsx
+++ client/src/component/FACULTY/CreateAndViewSubject/CreateSubject.jsx
@@ -29,17 +29,22 @@ const CreateSubject = (props) => {
     }
     return(
         <div style={{display:props.view}} className="dashbrd-40-colm">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Subject Name</label>
-                    <input type="text" placeholder="Enter Subject Name" value={name}
-                           onChange={(e) => setName(e.target.value)} required/>
-                </div>
+            
+            <form onSubmit={handleSubmit} style={{display:'grid',color:'#3c8dbc',backgroundColor:'azure',boxShadow:'0 0 5px grey'}}>
+            <p style={{fontSize:'17px'}}>Create New Subject </p>
+            <dl class="dl-horizontal">
+                    <dt><label>Subject Name</label></dt>
+                    <dd> <input type="text" placeholder="Enter Subject Name" value={name}
+                           onChange={(e) => setName(e.target.value)} required/> </dd>
+                
+             </dl>   
+                
+                
                 <span><button className="dashboard-btn dashboard-btn-scss"
                               type="submit">Submit</button></span>
             </form>
             <div>
-                <button style={{backgroundColor: 'lightseagreen'}} className="dashboard-btn dashboard-btn-scss" onClick={handleView}>
+                <button style={{backgroundColor: 'lightseagreen',marginTop:'20px'}} className="dashboard-btn dashboard-btn-scss" onClick={handleView}>
                     View / Delete Subject
                 </button>
             </div>
--- client/src/component/FACULTY/CreateAndViewSubject/ViewSubject.jsx
+++ client/src/component/FACULTY/CreateAndViewSubject/ViewSubject.jsx
@@ -30,24 +30,28 @@ const ViewSubject = (props) => {
     }
     const handleCancel = () => {
         setData([])
+        if(view=='block'){
+            setView('none')
+        }
+
     }
     return (
         <div className="dashbrd-40-colm" style={{display: view}}>
-           <button style={{float:'left'}}className="dashboard-btn dashboard-btn-scss excel-btn" onClick={handleCancel}>Clear Result</button>
+           <button style={{float:'right'}}className="dashboard-btn dashboard-btn-scss excel-btn" onClick={handleCancel}>Clear Result</button>
             <table className="table-60">
                 <thead>
                 <tr>
-                    <th>Id</th>
+                    <th>Sl. No.</th>
                     <th>Subject Name</th>
                     <th>Delete</th>
                 </tr>
                 </thead>
                 <tbody>
                 {
-                    data.map((item) => {
+                    data.map((item,idx) => {
                         return (
                             <tr key={item.id}>
-                                <td>{item.id}</td>
+                                <td>{idx+1}</td>
                                 <td>{item.subject}</td>
                                 <td>
                                     <button className="dashboard-btn dashboard-btn-scss btn-warning"
@@ -60,7 +64,7 @@ const ViewSubject = (props) => {
                 }
                 </tbody>
             </table>
-            {data.length===0 ? <div className="no-data">No Data Exists</div> : null}
+            {data.length===0 ? <div className="no-data" style={{textAlign:'center',width:'100%'}}>No Data Exists</div> : null}
         </div>
     )
 }
