e5640229191735c3155811c8667f1e2742d3acd5
indranil <indranilnonofficial007@gmail.com>
2024-03-08 17:08:03 +0530

    ui update 17-12-2023 gallery-final-4

--- client/src/component/STOCKUSAGE/StockUsageView/StockUsageView.jsx
+++ client/src/component/STOCKUSAGE/StockUsageView/StockUsageView.jsx
@@ -3,10 +3,12 @@ import {useEffect, useState} from "react";
 const StockUsageView = (props) => {
     const [view, setView] = useState("none");
     const [stockUsage, setStockUsage] = useState([]);
+    
 
     useEffect(() => {
         if(props.view === "block" && props.data.length > 0){
             setView("block");
+            
         }
         else{
             setView("none");
@@ -19,6 +21,15 @@ const StockUsageView = (props) => {
     let StockUsages = 0;
     let StockBuys = 0;
     let StockLeft = 0;
+
+    const clearTable = () => {
+        setStockUsage([]);
+       
+        
+      };
+
+    
+
     stockUsage.map((stock, index) => {
         if(stock.type === "Minus"){
             StockUsages += stock.quantity;
@@ -31,14 +42,19 @@ const StockUsageView = (props) => {
 
     return(
         <div style={{display:view}}>
+            <button className="dashboard-btn dashboard-btn-scss excel-btn" onClick={clearTable}>Clear Result</button>
             <table className="table-60">
                 <thead>
                     <tr>
-                        <th>Index</th>
-                        <th>Stock Name</th>
-                        <th>Quantity</th>
-                        <th>Unit</th>
-                        <th>Usage Date</th>
+                        <th>Sl. No.</th>
+                        <th>Item Name</th>
+                        <th>Date of Entry/Usage</th>
+                        
+                        <th>Type (Entry/Usage)</th>
+                        
+                        <th style={{width:'10%'}}>Amount(Pc./Kg./Ltr./Mtr.)</th>
+                        
+                        
                     </tr>
                 </thead>
                 <tbody>
@@ -47,28 +63,44 @@ const StockUsageView = (props) => {
                         <tr key={index}>
                             <td>{index + 1}</td>
                             <td>{stock.item_name}</td>
-                            <td>{stock.quantity}</td>
-                            <td>{stock.type==="Plus" ? "Entry":"Use"}</td>
                             <td>{stock.entry_date.slice(0, 10)}</td>
+                            
+                            <td>{stock.type==="Plus" ? "Entry":"Usage"}</td>
+                            
+                            <td>{stock.type==="Plus" ? "+":"-"}{stock.quantity}</td>
+                            
+                            
                         </tr>
                     )
                 })}
-                <tr>
-                    <td>Total Buy</td>
-                    <td>{StockBuys}</td>
+               <tr ></tr>
+                <tr >
                     <td></td>
-                    <td>Total Stock Usage</td>
-                    <td> {StockUsages}</td>
+                    <td></td>
+                    <td></td>
+                    <td style={{backgroundColor:'#f39c12',color:'white',border:'1px solid black'}}><b>Total Entry Amount :</b></td>
+                    <td style={{border:'1px solid black'}}>{StockBuys}</td>
+                    
                 </tr>
-                <tr>
-                    <td></td>
-                    <td>Stock Left</td>
-                    <td>{StockLeft}</td>
+                <tr >
                     <td></td>
                     <td></td>
+                    <td></td>
+                    <td style={{backgroundColor:'#f39c12',color:'white',border:'1px solid black'}}><b>Total Usage Amount :</b></td>
+                    <td style={{border:'1px solid black'}}>{StockUsages}</td>
+                    
+                </tr>
+                <tr >
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td style={{backgroundColor:'#f39c12',color:'white',border:'1px solid black'}}><b>Total Left Amount :</b></td>
+                    <td style={{border:'1px solid black'}}>{StockLeft}</td>
+                    
                 </tr>
                 </tbody>
             </table>
+            {stockUsage.length===0 ? <div className="no-data">No Data Exists</div> : null}
         </div>
     )
 }
