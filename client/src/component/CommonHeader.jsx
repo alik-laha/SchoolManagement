7ad735a75810967eb80410c554b5944dbebcddde
indranil <indranilnonofficial007@gmail.com>
2024-03-31 18:20:23 +0530

    db query updated

--- client/src/component/CommonHeader.jsx
+++ client/src/component/CommonHeader.jsx
@@ -6,16 +6,16 @@ const CommonHeader = () => {
   return (
     <div className="navbar alhilal-header_color-css text-white  px-12 ">
       <div className="flex-1">
-      <img src={logo} alt='logo image' height ={90} width={90}/> 
+      <img src={logo} alt='logo image' height ={90} width={90} className="responsive-header-image"/> 
             <div style={{display: "grid"}}>
-            <span className='px-12 text-2xl' >AL HILAL MISSION</span>
+            <span className='px-12 text-2xl responsive-header-1' >AL HILAL MISSION</span>
             <span className='px-12 text-1xl' >An Ideal Educational Cultural & Social Welfare Organization</span>
             </div>
        {/*<a href="#" className="btn btn-ghost normal-case text-1xl">CodeBun</a>*/}
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0 font-bold ">
-          <li >
+          <li className="responsive-common-header-li">
           <NavLink to="/">Back to Home</NavLink>
           </li>
 
