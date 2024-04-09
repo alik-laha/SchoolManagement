b0425265061520fe922ad85496b01903a1ee0c16
indranil <indranilnonofficial007@gmail.com>
2024-04-09 07:00:57 +0530

    db query updated

--- client/src/component/NavLinkComponents/Facilities.jsx
+++ client/src/component/NavLinkComponents/Facilities.jsx
@@ -23,18 +23,27 @@ Peace be upon you,<br/><br/>
             </div>
        
         </div>
-        <div><br/>The Prophet Muhammad (PBUH) declared knowledge as the lost treasure of Muslims, urging them to seek it wherever it may be found. In 1999, under the open sky in Kadambagachhi, North 24 Parganas, a group of visionary and philanthropic individuals initiated a humble effort to combat the dearth of education in the region. Today, its reach extends from the sea to the mountains - encompassing areas like Bankura, Purulia, Koch Bihar, Birbhum, North Dinajpur, and South Dinajpur. Al-Hilal Mission has become a cradle for nurturing talents who have
+        <div><br/>&nbsp;&nbsp;&nbsp;The Prophet Muhammad (PBUH) declared knowledge as the lost treasure of Muslims, urging them to seek it wherever it may be found. In 1999, under the open sky in Kadambagachhi, North 24 Parganas, a group of visionary and philanthropic individuals initiated a humble effort to combat the dearth of education in the region. Today, its reach extends from the sea to the mountains - encompassing areas like Bankura, Purulia, Koch Bihar, Birbhum, North Dinajpur, and South Dinajpur. Al-Hilal Mission has become a cradle for nurturing talents who have
          excelled in various fields, including medicine, engineering, academia, and more, contributing to societal development.
 <br/>
 <br/>
-Acknowledging the lack of financial resources for those grappling with daily sustenance, Al-Hilal Mission has provided a conducive environment and care for gifted children, ensuring they don't lose their potential due to economic hardships.
+&nbsp;&nbsp;&nbsp;Acknowledging the lack of financial resources for those grappling with daily sustenance, Al-Hilal Mission has provided a conducive environment and care for gifted children, ensuring they don't lose their potential due to economic hardships.
 Warm regards,
-<br/>
-<br/>
 We take immense pride and joy in the recognition bestowed upon us by the current state government as a Non-financial Aided educational institution. This acknowledgment, as per Memo No. 283-E(E)/10M-93/2013 dated 19.03.2013, is a testament to our commitment and contributions.
 <br/>
 <br/>
-
+&nbsp;&nbsp;&nbsp;Our gratitude extends to those individuals who, through their benevolence and generosity, have played pivotal roles in the establishment and growth of Al-Hilal Mission. We owe a debt of gratitude to Alhaj Mostak Hossain, Alhaj Shahajan Biswas, Alhaj M. Khalil, Alhaj M.A. Rashid, late Alhaj Ashraf Ali, Alhaj Alamgir Fakir, Alhaj Abul Khayer Chowdhury, Alhaj Ekramul, Abdur Rahman, Janab Asadujjaman, Osman Saheb, Ikram Lodhi, and many other anonymous philanthropists.
+In the face of the COVID-19 pandemic, the former advisors, late Partho Sen Gupta, late Shri Prabir Bhattacharya, late Abdur Rouf, Dr. Shamsul Islam, Shri Ashok Guha, Ziaul Haque, Shahidul Islam, Shamsul Alam, Alhaj Lokman Hakim, Janab Alhaj Shahabuddin, and Janab Amzad Hossain, have provided invaluable advice to shape Al-Hilal Mission's response to the crisis.
+<br/>
+<br/>
+&nbsp;&nbsp;&nbsp;Lastly, we extend our sincere appreciation to all who have generously supported us through their donations, wisdom, and prayers. We pray to Allah for your well-being and express our gratitude for your continued support.
+<br/>
+<br/>
+With prayers,<br/><br/>
+Shahabuddin Gaine, <br/>
+General Secretary,<br/>
+Al-Hilal Mission <br/>
+Kadambagachi 
 
 
 </div>
