fa967a8c18d38d868dd90608121f17e134aad6b5
indranil <indranilnonofficial007@gmail.com>
2024-04-09 07:36:38 +0530

    db query updated

--- client/src/component/NavLinkComponents/Programs.jsx
+++ client/src/component/NavLinkComponents/Programs.jsx
@@ -8,7 +8,34 @@ const Programs = (props) => {
     return (
         <div>
             <CommonHeader/>
-            About US
+        <div className='about-us-main text-1xl'>
+        <div className='about-us-introduction'>
+        
+
+        Our institution prides itself on a distinctive approach to education based on the following key features:
+        </div>
+        
+        <div className='about-us-objectives'>
+        
+        <p><b>1.	Curriculum Aligned with West Bengal Board: </b></p> <br/>
+        Our curriculum is designed in accordance with the standards set by the West Bengal Council of Higher Secondary Education and the Higher Secondary Education Parliament. Experienced teachers ensure the delivery of scientifically approved lessons.<br/>
+<br/><p><b>2.	Specialized Training for Vulnerable Students: </b></p> <br/>
+We provide special training at the primary level to support and uplift academically weaker students, ensuring that no one is left behind.<br/>
+<br/><p><b>3.	Regular Examinations and Parental Involvement: </b></p> <br/>
+Regular examinations and assessments are conducted to track student progress. Parental involvement is encouraged to foster a collaborative learning environment.<br/>
+<br/><p><b>4.	Islamic Education and Practices: </b></p> <br/>
+Alongside academic pursuits, we emphasize the importance of Islamic education, including regular prayers (five times a day), teaching Islamic etiquette and jurisprudence, and providing instruction on the Quran.<br/>
+<br/><p><b>5.	Promoting Success in Competitive Examinations:</b></p> <br/>
+•	Fostering a culture of academic excellence by encouraging students to excel in nationwide competitive exams.<br/>
+•	Recognizing successful achievements as a stepping stone toward future educational endeavors.<br/>
+<br/><p><b>6.	Encouraging Participation in Diverse Fields of Higher Education:</b></p> <br/>
+•	Advocating for efforts to excel in diverse fields such as medical, engineering, and administrative positions at the national level.<br/>
+•	Nurturing an environment that supports and facilitates the pursuit of higher education across various disciplines.
+In summary, the outlined objectives reflect a visionary roadmap for societal development, emphasizing the pivotal role of education as a driver for economic growth, cultural preservation, ethical development, and public welfare. By addressing various facets of human development, the initiative aims to create a well-rounded and empowered society capable of meeting the challenges of the contemporary world.
+<br/>
+        </div>
+
+        </div>
             <Footer/>
         </div>
        
