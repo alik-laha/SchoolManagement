92452cd7c5bbf527603ec1f69fbdf76211cc572a
indranil <indranilnonofficial007@gmail.com>
2024-04-10 09:50:33 +0530

    db query updated

--- client/src/component/Home/PaymentTab.jsx
+++ client/src/component/Home/PaymentTab.jsx
@@ -11,6 +11,11 @@ const PaymentTab=()=>{
     return(
        <div className="payment-tab-main">
         <span className="payment-header">Donation / Pay  </span>
+        <span className='payment-bank-txt'><b>Account Number :</b> 82940200001404<br/>
+            <b>Branch Name :</b> KADAMBAGACHI<br/>
+            <b>IFSC :</b> BARBOVJKADA<br/>
+            <b>MICR :</b> 700012195<br/>
+            </span>
         <img src={qr} alt='logo image' className='qr-pay-img'/> 
        </div>
     )
