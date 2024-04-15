c10a2282053326209883eae870b7dafd62b8ca05
indranil <indranilnonofficial007@gmail.com>
2024-04-15 06:34:23 +0530

    db query updated

--- client/src/component/NavLinkComponents/DownloadForms.jsx
+++ client/src/component/NavLinkComponents/DownloadForms.jsx
@@ -19,42 +19,29 @@ const DownloadForms = ()=>{
         </thead>
         <tbody>
             <tr>
-                <td>5:30 AM </td>
+                <td>1</td>
                 <td>Waking Up and Morning Routine</td>
             </tr>
             <tr>
-                <td>5:45 AM </td>
+                <td>2</td>
                 <td>Fajr Prayer and Quran Recitation</td>
             </tr>
             
             <tr>
-                <td>6:15 AM </td>
+                <td>3 </td>
                 <td>Physical Exercise and Prayer</td>
             </tr>
             <tr>
-                <td>6:30 AM </td>
+                <td>4 </td>
                 <td>Breakfast</td>
          
             </tr>
             <tr>
-                <td>6:45 AM - 9:45 AM </td>
+                <td>5 </td>
                 <td>Coaching/Prep Classes</td>
           
             </tr>
-            <tr>
-                <td>	9:45 AM - 10:40 AM </td>
-                <td>Bath and Morning Meal</td>
-            </tr>
-            <tr>
-                <td>	10:50 AM 	  </td>
-                <td>National Anthem</td>
-               
-            </tr>
-            <tr>
-                <td>	11:00 AM - 3:45 PM 	  </td>
-                <td>Study Session</td>
-        
-            </tr>
+          
           
            
            
