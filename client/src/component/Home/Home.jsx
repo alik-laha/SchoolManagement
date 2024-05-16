d82e8f399afc5a40f8d0a95f7b73d153d2e6c12f
indranil <indranilnonofficial007@gmail.com>
2024-05-16 09:22:29 +0530

    db query updated

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -1,19 +1,41 @@
 // src/components/Home/Home.js
 // import CSS
 import './Home.css';
-import React from 'react';
+import React, { useState ,useEffect} from 'react';
 import background from './school_image.jpg';
+import img4 from '../Galllery/GalleryImage/img1.jpg'
+import img2 from '../Galllery/GalleryImage/img9.jpeg'
+import img3 from '../Galllery/GalleryImage/facultyimg1.jpg'
 import NoticeBoardRenderHome from "../NoticeOutput/NoticeBoardRenderHome.jsx";
 import PaymentTab from './PaymentTab.jsx';
 
 
 const Home = (props) => {
     const ismob= window.innerWidth<768;
+    const [bgimage,setBgimage]=useState(background)
+    const images= [img3,img4,img2];
+    const intervalduration= 7000
+
+
+    useEffect(()=>{
+        const interval = setInterval(() => {
+          //do something here
+
+            const currentindex= images.indexOf(bgimage)
+            const nextindex=(currentindex+1)%images.length;
+            setBgimage(images[nextindex])
+
+        
+        }, intervalduration);
+
+           return ()=> clearInterval(interval)
+      },[bgimage,images])
+
 
     return (
         
         <div >
-            <div style={{ backgroundImage: `url(${background})`}} className="banner-container">
+            <div style={{ backgroundImage: `url(${bgimage})`}} className="banner-container">
                 {!ismob && <div className="payment-tab">
                     <PaymentTab/>
                     
